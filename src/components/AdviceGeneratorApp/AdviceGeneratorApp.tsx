3724adac474f97042344977b04cbe4d93605826a
Alan <pinhoneibor32@gmail.com>
2023-09-14 15:30:52 -0600

    feat: main component creation

---
+++ src/components/AdviceGeneratorApp/AdviceGeneratorApp.tsx
@@ -0,0 +1,10 @@
+export const AdviceGeneratorApp = () => {
+  return (
+    <main>
+      <h4>Advice # 117</h4>
+      <h1>Phrase here</h1>
+      <img src="/assets/images/pattern-divider-desktop.svg" alt="divider" />
+      <img src="/assets/images/icon-dice.svg" alt="icon-dice" />
+    </main>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { AdviceGeneratorApp } from './components/AdviceGeneratorApp/AdviceGeneratorApp';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AdviceGeneratorApp />
+  </React.StrictMode>,
+);
