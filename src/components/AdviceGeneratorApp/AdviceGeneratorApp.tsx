243e030cb4ef8fa886b110fb856b11e672ef7a73
Alan <pinhoneibor32@gmail.com>
2023-09-18 23:20:09 -0600

    chore: uptated messages and added button to refresh advices

--- src/components/AdviceGeneratorApp/AdviceGeneratorApp.tsx
+++ src/components/AdviceGeneratorApp/AdviceGeneratorApp.tsx
@@ -3,18 +3,23 @@ import { useFetchAdvices } from "../../helpers/useFetchAdvices"
 
 export const AdviceGeneratorApp = () => {
 
-  const { advice, error, isLoading } = useFetchAdvices();
+  const { advice, error, isLoading, getTips } = useFetchAdvices();
+
+  if(isLoading) return <h1>Loading Advice...</h1>
+
+  if(error) return <h1>Network request failed</h1>
 
   return (
     <main>
 
-      <h4>Advice # {isLoading ? '' : advice?.id}</h4>
-      {isLoading && <p>Loading Advice...</p>}
-      {error && <p>Network request failed</p>}
+      <h4>Advice # {advice?.id}</h4>
       <h1>{advice?.advice}</h1>
       
       <img src="/assets/images/pattern-divider-desktop.svg" alt="divider" />
-      <img src="/assets/images/icon-dice.svg" alt="icon-dice" />
+      
+      <button onClick={getTips}>
+        <img src="/assets/images/icon-dice.svg" alt="icon-dice" />
+      </button>
 
     </main>
   )
