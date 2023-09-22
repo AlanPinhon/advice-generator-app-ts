7997f52a26c39c0649dc8b684b50846754736476
Alan <pinhoneibor32@gmail.com>
2023-09-22 15:21:48 -0600

    chore: styles in AdviceGeneratorApp

--- src/components/AdviceGeneratorApp/AdviceGeneratorApp.tsx
+++ src/components/AdviceGeneratorApp/AdviceGeneratorApp.tsx
@@ -1,23 +1,25 @@
-// import { getAdvices } from "../../helpers/getAdvices"
 import { useFetchAdvices } from "../../helpers/useFetchAdvices"
+import './AdviceGeneratorAppStyles.css';
 
 export const AdviceGeneratorApp = () => {
 
   const { advice, error, isLoading, getTips } = useFetchAdvices();
 
-  if(isLoading) return <h1>Loading Advice...</h1>
+  if(isLoading) return <h1 className="loading-text">Loading Advice...</h1>
 
-  if(error) return <h1>Network request failed</h1>
+  if(error) return <h1 className="error-msg">Network request failed</h1>
 
   return (
-    <main>
+    <main className="advice-app-container">
 
-      <h4>Advice # {advice?.id}</h4>
-      <h1>{advice?.advice}</h1>
+      <section>
+        <h4 className="advice-id">ADVICE #{advice?.id}</h4>
+        <h1 className="advice-text">"{advice?.advice}"</h1>
+      </section>
       
-      <img src="/assets/images/pattern-divider-desktop.svg" alt="divider" />
+      <img className="divider" src="/assets/images/pattern-divider-desktop.svg" alt="divider" />
       
-      <button onClick={getTips}>
+      <button className="advice-btn" onClick={getTips}>
         <img src="/assets/images/icon-dice.svg" alt="icon-dice" />
       </button>
 
